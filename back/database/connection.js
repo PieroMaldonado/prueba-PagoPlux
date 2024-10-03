05a6ea77f001f170fd0f39f578afc2586794c164
PieroMaldonado <pieromaldonado16@gmail.com>
2024-10-03 08:28:09 -0500

    feat(api): add db connection

---
+++ back/database/connection.js
@@ -0,0 +1,24 @@
+import sql from 'mssql'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const dbConfig = {
+    user: process.env.DB_USER,          
+    password: process.env.DB_PASSWORD,  
+    server: process.env.DB_SERVER,      
+    database: process.env.DB_DATABASE,  
+    options: {
+        encrypt: true,                  
+        trustServerCertificate: true
+    }
+};
+
+export const getConnection = async () => {
+    try {
+        await sql.connect(dbConfig);
+        console.log('Conectado a la base de datos SQL Server');
+    } catch (err) {
+        console.error('Error en la conexión a la base de datos:', err);
+    }
+};
--- back/index.js
+++ back/index.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import { getConnection} from './database/connection.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-const PORT = PROSS.env.PORT || 3000
+const PORT = process.env.PORT || 3000
+
+getConnection()
 
 app.get('/', (req, res) => {
     res.send('<h1>Hola mundo!</h1>')
