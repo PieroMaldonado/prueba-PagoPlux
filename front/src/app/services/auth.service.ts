2a23e2f89cf7395bf452c39ee0a878de7fe68c57
PieroMaldonado <pieromaldonado16@gmail.com>
2024-10-03 13:48:14 -0500

    feat: add interfaces and service to Angular

---
+++ front/src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { appsettings } from '../settings/appsettings';
+import { ILogin } from '../interfaces/ILogin';
+import { Observable } from 'rxjs';
+import { IResponseLogin } from '../interfaces/IResponseLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private http = inject(HttpClient)
+  private baseUrl:string = appsettings.apiUrl
+
+  constructor() { }
+
+  login(objeto:ILogin): Observable<IResponseLogin>{
+    return this.http.post<IResponseLogin>(`${this.baseUrl}login`, objeto)
+  }
+}
