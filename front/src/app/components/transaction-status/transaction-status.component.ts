a405c9d3f3107cf7a4fdcf6deb17524556d45808
PieroMaldonado <pieromaldonado16@gmail.com>
2024-10-06 13:16:00 -0500

    feat: add logic to transaction-status component

--- front/src/app/components/transaction-status/transaction-status.component.ts
+++ front/src/app/components/transaction-status/transaction-status.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { TransactionService } from '../../services/transaction.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-transaction-status',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './transaction-status.component.html',
   styleUrl: './transaction-status.component.css'
 })
 export class TransactionStatusComponent {
+  private transactionService = inject(TransactionService)
+  @Input() transactionId: string = ''
 
+  getTransactionStatus() {
+    // if(!this.transactionId) 
+    // {
+    //   alert('Aún no realiza una transacción');
+    // }
+
+    const transactionId = 'e530361c-2145-4e0f-84e1-70cce4ff4a91';
+    this.transactionService.getTransactionById({ idTransaction: transactionId }).subscribe({
+      next: (response) => {
+        console.log('Transaction Response:', response);
+      },
+      error: (err) => {
+        console.error('Error fetching transaction status:', err);
+      }
+    });
+  }
 }
--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PagopluxButtonComponent } from '../../components/pagoplux-button/pagoplux-button.component';
+import { TransactionStatusComponent } from '../../components/transaction-status/transaction-status.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PagopluxButtonComponent],
+  imports: [PagopluxButtonComponent, TransactionStatusComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
